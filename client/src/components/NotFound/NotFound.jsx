733aa48d435154bdde7e99748aed5163a4252cef
Cavan P <53267753+Cavan-P@users.noreply.github.com>
2025-02-23 13:10:52 -0600

    dale

--- client/src/components/NotFound/NotFound.jsx
+++ client/src/components/NotFound/NotFound.jsx
@@ -1,11 +1,20 @@
 import { Link } from 'react-router-dom'
+import Dale from '../../assets/Dale_nobackground.png'
+
+import './NotFound.css'
 
 const NotFound = () => {
     return (
         <div style={{ textAlign: 'center', marginTop: '50px' }}>
             <h1>404</h1>
             <p>i think you got lost</p>
+            <p>say hi to dale, then head back to the home page</p>
             <Link to="/">home</Link>
+            <div className="dale">
+                <h3>Dale</h3>
+                <i className="arrow" />
+                <img src={Dale} alt="Dale" />
+            </div>
         </div>
     )
 }
